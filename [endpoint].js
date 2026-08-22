bf2f734960277fcfb08fdc28955fb144e944cf5e
masprew <ardiansyahferri530@gmail.com>
2026-08-22 18:33:41 +0700

    Delete [endpoint].js

--- [endpoint].js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    success: true,
-    message: 'API function OK',
-    method: req.method,
-    endpoint: req.query.endpoint || null
-  });
-}
